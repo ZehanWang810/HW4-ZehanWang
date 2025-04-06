060a4913f6e747599457e60bc9e758930e2d200c
ZehanWang810 <wangzehannb@163.com>
2025-04-06 09:13:29 -0800

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Socket.io Chat</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
